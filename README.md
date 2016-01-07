00577f9f6ef8f14af927d567db74dacf8452ec67
i3visio <contacto@i3visio.com>
2016-01-08 00:33:04 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,46 @@
+     _ _____      _     _
+    (_)___ /_   _(_)___(_) ___         ___ ___  _ __ ___
+    | | |_ \ \ / / / __| |/ _ \       / __/ _ \| '_ ` _ \
+    | |___) \ V /| \__ \ | (_) |  _  | (_| (_) | | | | | |
+    |_|____/ \_/ |_|___/_|\___/  (_)  \___\___/|_| |_| |_|
+
+    ========================================================================
+    Web Surfer Assistant  Copyright (C) 2016  F. Brezo and Y. Rubio, i3visio
+    ========================================================================
+
+[![License](https://img.shields.io/badge/license-GNU%20General%20Public%20License%20Version%203%20or%20Later-blue.svg)]()
+
+Description:
+============
+Punycode Alert is a Google Chrome, Chromium, Opera extension that warns users when the URL 
+they are accessing has some Punycode content.
+
+License: GPLv3+
+===============
+
+This is free software, and you are welcome to redistribute it under certain conditions.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+For more details on this issue, check the LICENSE file.
+
+The documentation of this program included in the doc folder is ruled by
+GNU Free Document License v1.3.
+
+Installation:
+=============
+Load it from the Chrome Store or drag and drop the .crx file onto the browser
+under the chrome://extensions. That should install it in Google Chrome, Chromium and
+Opera.
---
+++ js/background.js
@@ -0,0 +1,59 @@
+/*
+     _ _____      _     _
+    (_)___ /_   _(_)___(_) ___         ___ ___  _ __ ___
+    | | |_ \ \ / / / __| |/ _ \       / __/ _ \| '_ ` _ \
+    | |___) \ V /| \__ \ | (_) |  _  | (_| (_) | | | | | |
+    |_|____/ \_/ |_|___/_|\___/  (_)  \___\___/|_| |_| |_|
+
+    Copyright 2016 Félix Brezo and Yaiza Rubio (i3visio, contacto@i3visio.com)
+
+    This file is part of phishify. You can redistribute it and/or 
+    modify it under the terms of the GNU General Public License as published 
+    by the Free Software Foundation, either version 3 of the License, or (at 
+    your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but 
+    WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
+    Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// To change when the request is performed
+chrome.extension.onRequest.addListener(function(request, sender) {
+    chrome.tabs.query({active: true}, function(tabArray) {
+        chrome.browserAction.setBadgeText({"text": ""});
+        var punycodeStr = "xn--";
+        // Cogemos la URL de la pestaña activa
+        var currentURL = tabArray[0].url;
+
+        // Si matchea con la objetivo... 
+        if (currentURL.indexOf(punycodeStr) > -1) {
+            alert("BE CAREFUL! Is this URL the one you want to go?\n\nIt has some punycode content:\n" + currentURL);
+            // Mostrando el cambio en un texto
+            chrome.browserAction.setBadgeText({"text": "Wait"});
+        }
+    });
+});
+
+//To change when the selection changes...
+chrome.tabs.onSelectionChanged.addListener(function(tabId, props) {
+
+    chrome.tabs.query({active: true}, function(tabArray) {
+        chrome.browserAction.setBadgeText({"text": ""});
+        var punycodeStr = "xn--";
+        // Cogemos la URL de la pestaña activa
+        var currentURL = tabArray[0].url;
+
+        // Si matchea con la objetivo... 
+        if (currentURL.indexOf(punycodeStr) > -1) {
+            alert("BE CAREFUL! Is this URL the one you want to go?\n\nIt has some punycode content:\n" + currentURL);
+            // Mostrando el cambio en un texto
+            chrome.browserAction.setBadgeText({"text": "Wait"});
+        }
+    });
+});
+
