7eb506e29e3e563020f086f728cdad77ba6edc98
i3visio <contacto@i3visio.com>
2016-01-08 00:33:04 +0100

    Initial commit.

---
+++ js/content.js
@@ -0,0 +1,26 @@
+/*
+     _ _____      _     _
+    (_)___ /_   _(_)___(_) ___         ___ ___  _ __ ___
+    | | |_ \ \ / / / __| |/ _ \       / __/ _ \| '_ ` _ \
+    | |___) \ V /| \__ \ | (_) |  _  | (_| (_) | | | | | |
+    |_|____/ \_/ |_|___/_|\___/  (_)  \___\___/|_| |_| |_|
+
+    Copyright 2016 Félix Brezo and Yaiza Rubio (i3visio, contacto@i3visio.com)
+
+    This file is part of phishify. You can redistribute it and/or 
+    modify it under the terms of the GNU General Public License as published 
+    by the Free Software Foundation, either version 3 of the License, or (at 
+    your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but 
+    WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
+    Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+chrome.extension.sendRequest({redirect: true});
+
